500fd720c9a143568671a82fa32a2d92d3657451
tonylandamx <tonylandamx@yahoo.com.mx>
2024-04-21 11:57:03 -0500

    "Initial commit"

---
+++ evenOrOdd.test.js
@@ -0,0 +1,12 @@
+const { evenOrOdd } = require("./evenOrOdd.js");
+
+describe('isEvenOrOdd', () => {
+    test('returns Even for even number', () => {
+        expect(evenOrOdd(24)).toBe("Even");
+        expect(evenOrOdd(10)).toBe("Even");
+    });
+    test('returns Odd for odd number', () => {
+        expect(evenOrOdd(33)).toBe("Odd");
+        expect(evenOrOdd(1)).toBe("Odd");
+    });
+});
